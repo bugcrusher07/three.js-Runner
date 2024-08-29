c45a1b28a04a3a4200e073ba8a076810951acd25
bugcrusher07 <tarun.develops@gmail.com>
2024-08-29 07:53:24 +0530

    i don't even know what i am doing anymore

--- threeApp.js
+++ threeApp.js
@@ -1,7 +1,9 @@
-import * as THREE from "three";
+ import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight , 0.1 , 1000);
+const scene = new THREE.Scene();o
+
+const camera = new THREE.PerspectiveCamera(100,window.innerWidth/window.innerHeight , 0.1 , 1000);
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -11,7 +13,12 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color:0x00ff00});
 const cube = new THREE.Mesh(geometry,material);
-scene.add(cube);
+
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("./res/FPS_player.gltf",(gltfScene) => {
+  scene.add(gltfScene);
+})
 
 camera.position.z =5;
 
